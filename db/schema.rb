1e56c875e747fe86a3d81418fe95c0693baeca7b
Arka Ganguli <arkaganguli1@gmail.com>
2017-03-07 20:44:00 -0500

    add the required columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209171407) do
+ActiveRecord::Schema.define(version: 20170308001010) do
 
   create_table "love_letters", force: :cascade do |t|
     t.string   "title"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20170209171407) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "is_president"
+    t.boolean  "is_exec"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
