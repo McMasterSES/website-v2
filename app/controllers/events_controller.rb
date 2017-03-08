70f940296343862ea49f51892f3b72fdcbfad530
Sadisha Galappatti <sadishag@gmail.com>
2017-03-07 19:06:40 -0500

    Added boiler plate for crud methods of events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,24 @@
 class EventsController < ApplicationController
+
+    # Index is the main events page
+    # We can use this as a method to list all of the events available
     def index
     end
+
+    # This method will be used to create new events and store them to our db
+    def create
+    end
+
+    # Update event used to Edit individual events 
+    def update
+    end
+
+    # Used to display one single event, usually routed from the big list of all events
+    def show
+    end
+
+    # Used to delete a specific event, we will just need the event id to delete it from the db
+    def delete
+    end
+
 end
